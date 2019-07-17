5abceaebe7947f1386020dd10f141abf5473e7cb
Tylor Schafer <tylorschafer@gmail.com>
2019-07-17 14:17:45 -0600

    Add patron test functions

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -6,7 +6,23 @@ require './lib/patron'
 class PatronTest < Minitest:: Test
 
   def setup
+    @bob = Patron.new("Bob", 20)
+  end
 
+  def test_it_exists
+    assert_instance_of Patron, @bob
+  end
+
+  def test_attributes
+    assert_equal "Bob", @bob.name
+    assert_equal 20, @bob.spending_money
+    assert_equal [], @bob.interests
+  end
+
+  def test_add_interest
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @bob.interests
   end
 
 end
