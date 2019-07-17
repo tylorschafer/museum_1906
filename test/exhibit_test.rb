eef2c6f0f8264a4f578e3021ea9a941c17b32da7
Tylor Schafer <tylorschafer@gmail.com>
2019-07-17 14:11:14 -0600

    Add initial iteration 1 test files

---
+++ test/exhibit_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+
+class ExhibitTest < Minitest:: Test
+
+  def setup
+
+  end
+
+end
---
+++ test/patron_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+
+class PatronTest < Minitest:: Test
+
+  def setup
+
+  end
+
+end
